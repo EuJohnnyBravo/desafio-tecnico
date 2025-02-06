4ee179c9891835c1760538c4ebbf9d12b80d2c99
Rodrigo Oliveira <rodrigo.silva.olviera369@gmail.com>
2025-02-06 16:04:27 -0300

    add project references for Domain, Communication, and Exception layers in Application and Infrastructure projects

--- WebScrapping-Backend/src/WebScrapping.Infrastructure/obj/Debug/net6.0/WebScrapping.Infrastructure.AssemblyInfo.cs
+++ WebScrapping-Backend/src/WebScrapping.Infrastructure/obj/Debug/net6.0/WebScrapping.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebScrapping.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e10550db37a1e7f62e1ca144eccece952caefc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebScrapping.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebScrapping.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebScrapping-Backend/src/webscrapping.Application/obj/Debug/net6.0/webscrapping.Application.AssemblyInfo.cs
+++ WebScrapping-Backend/src/webscrapping.Application/obj/Debug/net6.0/webscrapping.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebScrapping.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e10550db37a1e7f62e1ca144eccece952caefc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebScrapping.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebScrapping.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
