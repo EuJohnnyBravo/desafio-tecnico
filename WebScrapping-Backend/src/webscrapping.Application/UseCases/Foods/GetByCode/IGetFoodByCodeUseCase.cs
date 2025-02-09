ee90f344add8fff248ed22d03ed83231b0d42d15
Rodrigo Oliveira <rodrigo.silva.olviera369@gmail.com>
2025-02-09 15:10:14 -0300

    Implement GetFoodByCode use case and API endpoint in FoodController

---
+++ WebScrapping-Backend/src/webscrapping.Application/UseCases/Foods/GetByCode/IGetFoodByCodeUseCase.cs
@@ -0,0 +1,8 @@
+﻿using WebScrapping.Communication.Responses;
+
+namespace WebScrapping.Application.UseCases.Foods.GetByCode;
+
+public interface IGetFoodByCodeUseCase
+{
+    Task<ResponseSingleFoodJson> Execute(string code);
+}
